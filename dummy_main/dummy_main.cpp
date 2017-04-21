299f52ed7c4865dbb9361c529a7803ba907052be
Rian Quinn <rianquinn@gmail.com>
2017-04-21 13:06:23 -0600

    updates from bfsdk
    Signed-off-by: Rian Quinn <“rianquinn@gmail.com”>

--- dummy_main/dummy_main.cpp
+++ dummy_main/dummy_main.cpp
@@ -19,10 +19,12 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include <bfgsl.h>
+#include <bftypes.h>
 #include <bfexports.h>
 
-#include <exception>
+#include <cstring>
+#include <stdexcept>
+
 #include <dummy_libs.h>
 
 derived1 g_derived1;
